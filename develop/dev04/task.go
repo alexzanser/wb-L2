eab6eb513629904299087c845ff30f41e0d2ff9e
tolkachev <alexzanser@mail.ru>
2022-05-15 19:13:49 +0300

    1

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func Letters(word string) map[rune]struct{} {
+func letters(word string) map[rune]struct{} {
 	letters := make(map[rune]struct{})
 	for _, l := range []rune(word) {
 		letters[l] = struct{}{}
@@ -16,7 +16,7 @@ func Letters(word string) map[rune]struct{} {
 	return letters
 }
 
-func Contains(words []string, word string) bool {
+func contains(words []string, word string) bool {
     for _, a := range words  {
         if a == word {
             return true
@@ -25,16 +25,16 @@ func Contains(words []string, word string) bool {
     return false
 }
 
-func Anagrams(words []string) *map[string][]string {
+func anagrams(words []string) *map[string][]string {
 	anagrams := make(map[string][]string)
 	for _, word := range words {
 		hasSet := false
 		word = strings.ToLower(word)
 		
 		for key := range anagrams {
-			if reflect.DeepEqual(Letters(word), Letters(anagrams[key][0])) {
+			if reflect.DeepEqual(letters(word), letters(anagrams[key][0])) {
 				hasSet = true
-				if Contains(anagrams[key], word) == false {
+				if contains(anagrams[key], word) == false {
 					anagrams[key] = append(anagrams[key], word)
 				}
 			}
@@ -49,5 +49,5 @@ func Anagrams(words []string) *map[string][]string {
 
 func main() {
 	dict := []string{"Пятак", "пятка", "тяПка", "Листок", "слиток", "Столик", "123", "132"}
-	fmt.Println(Anagrams(dict))
+	fmt.Println(anagrams(dict))
 }
