0e2599b718e16dd8e76425ba467b9154443d01e7
Kortney Mercy <kmercy@pr-d5.kzn.21-school.ru>
2022-05-24 14:37:06 +0300

    1

--- develop/dev09/internal/wget/wget.go
+++ develop/dev09/internal/wget/wget.go
@@ -60,9 +60,9 @@ func (wget *Wget) GetPage(url string) error {
 		return fmt.Errorf("error when send request: %v", err)
 	}
 
-	createDir(wget.BasePath + getPath(url))
+	createDir(wget.BasePath + "/"+ getPath(url))
 
-	file, err  := os.Create(wget.BasePath + getPath(url) + "/" + getFileName(url))
+	file, err  := os.Create(wget.BasePath + "/" + getPath(url) + "/" + getFileName(url))
 
 	if err != nil {
 		return fmt.Errorf("error when create file: %v", err)
